710b1a4bd738d55af93502ea1608c7885212cedc
Tanso <tansomctexp@gmail.com>
2025-05-04 19:41:07 +0900

    [UPDATE]End of creation of function for type casting. Tests have been added.

---
+++ Minecraft_DirectX/WB_Utility/include/DataCast.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include "WB_Utility/include/config.h"
+
+#include <memory>
+
+namespace WB
+{
+
+/***********************************************************************************************************************
+ * As
+ * Casts an instance of type S to the specified type T.
+/**********************************************************************************************************************/
+
+template <typename T, typename S>
+T* As(S* source)
+{
+    T* target = dynamic_cast<T*>(source);
+    return target;
+}
+
+/***********************************************************************************************************************
+ * UniqueAs
+ * Casts an unique_ptr instance of type S to the specified type unique_ptr<T>
+ * Ownership is transferred.
+/**********************************************************************************************************************/
+
+template <typename T, typename S>
+std::unique_ptr<T> UniqueAs(std::unique_ptr<S>& source)
+{
+    T* target = dynamic_cast<T*>(source.get());
+
+    if (target) return std::unique_ptr<T>(static_cast<T*>(source.release()));
+    else return nullptr;
+}
+
+/***********************************************************************************************************************
+ * CastWithReturn
+ * Casts an instance of type unique_ptr to the specified type and 
+ * returns ownership to the cast source when it exits scope.
+ * Casting from one type of Cast to another is discouraged.WithReturn
+/**********************************************************************************************************************/
+
+template <typename D, typename S>
+class UTILITY_API CastWithReturn
+{
+private:
+    std::unique_ptr<S>& src_;
+    std::unique_ptr<D> casted_ = nullptr;
+
+public:
+    CastWithReturn(std::unique_ptr<S>& src) : src_(src)
+    {
+        D* dstPtr = dynamic_cast<D*>(src.get());
+        if (dstPtr) casted_.reset(dynamic_cast<D*>(src.release()));
+    }
+
+    ~CastWithReturn()
+    {
+        src_.reset(dynamic_cast<S*>(casted_.release()));
+    }
+
+    std::unique_ptr<D>& operator()() 
+    {
+        return casted_;
+    }
+};
+
+} // namespace WB
---
+++ Minecraft_DirectX/WB_Utility_Test/DataCast_Test.cpp
@@ -0,0 +1,55 @@
+﻿#include "pch.h"
+
+#include "WB_Utility/include/DataCast.h"
+#pragma comment(lib, "WB_Utility.lib")
+
+class IWeapon
+{
+public:
+    virtual ~IWeapon() {}
+};
+
+class Sword : public IWeapon
+{
+public:
+    int Use() const { return 1; }
+};
+
+TEST(UtilityDataCast, As)
+{
+    IWeapon* weapon = new Sword();
+
+    {
+        Sword* sword = WB::As<Sword>(weapon);
+        EXPECT_EQ(sword->Use(), 1);
+    }
+
+    delete weapon; // Clean up the allocated memory
+}
+
+TEST(UtilityDataCast, UniqueAs)
+{
+    std::unique_ptr<IWeapon> weapon = std::make_unique<Sword>();
+
+    {
+        std::unique_ptr<Sword> sword = WB::UniqueAs<Sword>(weapon);
+        EXPECT_EQ(sword->Use(), 1);
+    }
+
+    EXPECT_EQ(weapon, nullptr); // sword should be null after UniqueAs
+}
+
+TEST(UtilityDataCast, CastWithReturn)
+{
+    std::unique_ptr<IWeapon> weapon = std::make_unique<Sword>();
+
+    {
+        WB::CastWithReturn<Sword, IWeapon> sword(weapon);
+        EXPECT_EQ(sword()->Use(), 1);
+
+        EXPECT_EQ(weapon, nullptr); // weapon should be null after CastWithReturn
+    }
+
+    EXPECT_NE(weapon, nullptr); // weapon should be valid after CastWithReturn goes out of scope
+}
+
